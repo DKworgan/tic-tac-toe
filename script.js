3068e12beb795368a90cf568788e08f5d78c3c72
DKworgan <worganstlaurent@gmail.com>
2022-12-11 22:09:35 -0500

    set up basic tic-tac-toe cells with CSS

--- script.js
+++ script.js
@@ -17,5 +17,28 @@ Game module
 player object
     player sign: x or o 
 
-    
+
 */
+
+const displayGame = (() => {
+    const gameContainer = document.querySelector("#gameContainer");
+    const gridColumns = new Array();
+    const gridRows = new Array();
+
+for(let i = 0; i < 3; i++){
+    gridColumns[i] = document.createElement("div");
+    gridColumns[i].classList.add("gridColumns");
+    gridColumns[i].id = "col" + i;
+    gameContainer.appendChild(gridColumns[i]);
+
+    gridColumns[i].style.display = "flex";
+    gridColumns[i].style.flexDirection = "column";
+
+    for(let x = 0; x < 3; x++){
+        gridRows[x] = document.createElement("div");
+        gridRows[x].classList.add("gridRowDiv");
+        gridRows[x].id = "col" + i + "row" + x;
+        gridColumns[i].appendChild(gridRows[x]);
+    }
+}
+})();
