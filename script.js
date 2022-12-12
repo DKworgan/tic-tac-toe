00ed15903bb39d2f45f891a7f98257ff5df72f78
DKworgan <worganstlaurent@gmail.com>
2022-12-11 21:57:59 -0500

    set up boiler plate for project

---
+++ script.js
@@ -0,0 +1,21 @@
+/* 
+DESIGN
+
+
+display module 
+    handles dom elements
+    updates dom 
+    interacts with game module
+
+Game module
+    turn function
+    isThereWinner
+    Array of inputted values
+    interacts with player object
+    interacts with display module
+
+player object
+    player sign: x or o 
+
+    
+*/
